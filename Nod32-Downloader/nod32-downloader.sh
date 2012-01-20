bfca98345488f7b12c1aa260961bca8bc81357b8
cHa0sLordz <Moe@h4x0rbox.(none)>
2012-01-20 11:31:09 +0100

    Deletet Nod32 Script
    Nod32 Script entfernt da Esset einen eigenen Autoupdater zur Verfügung
    stellt.

--- Nod32-Downloader/nod32-downloader.sh
+++
@@ -1,3 +0,0 @@
-# zeile für windows & entfernen der Temporären daten
-
-./nod32-linkgrabber.pl > Nod32-AllLinks.txt && cat Nod32-AllLinks.txt | grep -m1 eav_nt32_deu > Nod32-links.txt && wget -i Nod32-links.txt && mv *eav_nt32_deu* /home/daten/Software/Windows/Nod32/Nod32-32Bit.exe && ./nod32-linkgrabber.pl > Nod32-AllLinks.txt && cat Nod32-AllLinks.txt | grep -m1 eav_nt64_deu > Nod32-links.txt && wget -i Nod32-links.txt && mv *eav_nt64_deu* /home/daten/Software/Windows/Nod32/Nod32-64Bit.exe && rm -R Nod32-AllLinks.txt && rm -R Nod32-links.txt
